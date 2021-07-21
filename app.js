05f933a13a6ef46d8571a35f58c9e082d4d7438c
catdude2000 <michael.g.pace@gmail.com>
2021-07-21 18:25:20 -0500

    added clock button and some js stuff

---
+++ app.js
@@ -0,0 +1,14 @@
+let house = "3845"
+
+let userChoice = prompt ('Do you want your phone to be yellow or yellow?');
+
+alert(userChoice)
+
+function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  document.getElementById("demo").innerHTML = myFunction(4, 3);
+
+<button onclick="document.getElementById('demo').innerHTML=Date()">The time is?</button>;
+
+<p id="demo"></p>;
