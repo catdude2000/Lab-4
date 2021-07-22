ca788aa347047cf1c13ec820597577de16cce41f
catdude2000 <michael.g.pace@gmail.com>
2021-07-22 16:00:39 -0500

    fixed mouse prompt with ;,wrote prompt for dog numbers

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ function colorchoice (){let userChoice = prompt ('What color would you like for 
 if (userChoice === "yellow") {
     alert('Nice Choice!');
 }
-else {
+else; {
     alert('Too bad, its yellow!');
 }
-}let reptext = prompt ("What's Mickey's last name?");
- while(reptext !== 'Mouse'); {
-   reptext = prompt('Try again!')}
-   alert(reptext)
+}  let reptext = prompt ("What's Mickey's last name?");
+    while(reptext !== 'Mouse') {
+   reptext = prompt('Try again!');}
+   alert(reptext);
+{
+let usernumber = prompt("How many dogs should I get");
+for (i=0; i < usernumber; i++) {
+    document.write('Dog+');
+}}
