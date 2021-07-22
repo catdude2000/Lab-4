bae97e5b705aa2dbc7779c186ba1a0aba6380a98
catdude2000 <michael.g.pace@gmail.com>
2021-07-21 20:24:02 -0500

    Added style and finalised jss

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 let house = "3845"
 
-let userChoice = prompt ('Do you want your phone to be yellow or yellow?');
-
-alert(userChoice)
-
-function myFunction(p1, p2) {
-    return p1 * p2;
-  }
-  document.getElementById("demo").innerHTML = myFunction(4, 3);
-
-<button onclick="document.getElementById('demo').innerHTML=Date()">The time is?</button>;
-
-<p id="demo"></p>;
+let userChoice = prompt ('What color would you like for your phone? Yellow or something else?');
+if (userChoice === "yellow") {
+    alert('Nice Choice!');
+}
+else {
+    alert('Too bad, its yellow!');
+}
+let userchoice = prompt ("How about I repeat what you type?")
+    alert(userchoice)
